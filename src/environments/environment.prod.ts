00c137f9641939dab61a33dcf589b64455956600
rasulacaldera <rasula.caldera@hotmail.com>
2022-05-14 22:27:13 +0530

    adding firebase and configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'findcure-6e23d',
+    appId: '1:171411839169:web:8f101003db6c2a8412dc40',
+    databaseURL: 'https://findcure-6e23d-default-rtdb.firebaseio.com',
+    storageBucket: 'findcure-6e23d.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAQBPlf1sFMMf2uDRQ3VM8NU0i_P9QPWqM',
+    authDomain: 'findcure-6e23d.firebaseapp.com',
+    messagingSenderId: '171411839169',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'findcure-6e23d',
+    appId: '1:171411839169:web:8f101003db6c2a8412dc40',
+    databaseURL: 'https://findcure-6e23d-default-rtdb.firebaseio.com',
+    storageBucket: 'findcure-6e23d.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAQBPlf1sFMMf2uDRQ3VM8NU0i_P9QPWqM',
+    authDomain: 'findcure-6e23d.firebaseapp.com',
+    messagingSenderId: '171411839169',
+  },
   production: false
 };
 
