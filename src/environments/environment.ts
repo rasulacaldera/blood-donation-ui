f1bb5cd6ee0fee8e51dbde30a941b8bba8200f1e
rasulacaldera <rasula.caldera@hotmail.com>
2022-05-14 22:09:40 +0530

    adding env config for firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,3 +7,13 @@ export const environment = {
 };
 
 export const SERVER = 'http://localhost:8080/v1';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQBPlf1sFMMf2uDRQ3VM8NU0i_P9QPWqM",
+  authDomain: "findcure-6e23d.firebaseapp.com",
+  databaseURL: "https://findcure-6e23d-default-rtdb.firebaseio.com",
+  projectId: "findcure-6e23d",
+  storageBucket: "findcure-6e23d.appspot.com",
+  messagingSenderId: "171411839169",
+  appId: "1:171411839169:web:8f101003db6c2a8412dc40"
+};
