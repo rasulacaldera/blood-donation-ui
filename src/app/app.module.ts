54211c702b02c07e8dc60372939c6bfcf0945ad4
rasulacaldera <rasula.caldera@hotmail.com>
2022-05-15 08:46:18 +0530

    firebase config for donor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/stats/stats.component.ts
+++ src/app/pages/stats/stats.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-stats',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsComponent implements OnInit {
 
-  constructor() { }
+  items: Observable<any[]>;
+  constructor(db: AngularFireDatabase) {
+
+    // const docRef = doc(this.firestore, "donors", "/");
+    // const docSnap = getDoc(docRef);
+
+    // console.log(getDoc(docRef))
+
+    // console.log(doc(this.firestore, "cities", "SF"))
+    // this.items = 
+    // console.log()
+    // db.collection("camp details").valueChanges().subscribe(res => {
+    //   console.log(res)
+    // });
+
+    this.items = db.list('donors').valueChanges();
+
+  }
 
   ngOnInit(): void {
+    // this.items = this.db.list('items').valueChanges();
   }
 
 }
